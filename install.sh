12b696b5571387aba056e7df719aca339f08acad
GERMAN IBARRA RODRIGUEZ <2024030429@upsin.edu.mx>
2026-07-08 22:49:06 -0700

    refactor: remove github actions and include precompiled assets

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+echo "Installing AutoQueuer..."
+
 CUSTOM_APPS_DIR="$(spicetify -c | xargs dirname)/CustomApps"
 APP_DIR="$CUSTOM_APPS_DIR/AutoQueuer"
 
 mkdir -p "$APP_DIR"
 
-curl -fsSL https://raw.githubusercontent.com/AlemanGerman/AutoQueuer/main/manifest.json -o "$APP_DIR/manifest.json"
-curl -fsSL https://raw.githubusercontent.com/AlemanGerman/AutoQueuer/main/icon.svg -o "$APP_DIR/icon.svg"
-
-curl -fsSL https://github.com/AlemanGerman/AutoQueuer/releases/latest/download/extension.js -o "$APP_DIR/extension.js"
+curl -fsSL https://raw.githubusercontent.com/AlemanGerman/AutoQueuer/main/dist/manifest.json -o "$APP_DIR/manifest.json"
+curl -fsSL https://raw.githubusercontent.com/AlemanGerman/AutoQueuer/main/dist/icon.svg -o "$APP_DIR/icon.svg"
+curl -fsSL https://raw.githubusercontent.com/AlemanGerman/AutoQueuer/main/dist/index.js -o "$APP_DIR/index.js"
 
 spicetify config custom_apps AutoQueuer
 spicetify apply
