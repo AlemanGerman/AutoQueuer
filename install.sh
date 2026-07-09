76bcd5be1d0d1da411de1df2dc645d15a9e4649a
GERMAN IBARRA RODRIGUEZ <2024030429@upsin.edu.mx>
2026-07-09 01:02:49 -0700

    fix: Icon problems

--- install.sh
+++ install.sh
@@ -8,8 +8,7 @@ APP_DIR="$CUSTOM_APPS_DIR/AutoQueuer"
 mkdir -p "$APP_DIR"
 
 curl -fsSL https://raw.githubusercontent.com/AlemanGerman/AutoQueuer/main/manifest.json -o "$APP_DIR/manifest.json"
-curl -fsSL https://raw.githubusercontent.com/AlemanGerman/AutoQueuer/main/icon.svg -o "$APP_DIR/icon.svg"
-curl -fsSL https://raw.githubusercontent.com/AlemanGerman/AutoQueuer/main/index.js -o "$APP_DIR/index.js"
+curl -fsSL https://raw.githubusercontent.com/AlemanGerman/AutoQueuer/main/css/icon.svg -o "$APP_DIR/css/icon.svg"curl -fsSL https://raw.githubusercontent.com/AlemanGerman/AutoQueuer/main/index.js -o "$APP_DIR/index.js"
 curl -fsSL https://raw.githubusercontent.com/AlemanGerman/AutoQueuer/main/extension.js -o "$APP_DIR/extension.js"
 curl -fsSL https://raw.githubusercontent.com/AlemanGerman/AutoQueuer/main/style.css -o "$APP_DIR/style.css"
 
