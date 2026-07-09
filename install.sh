eed2697953a5a054448602ded027af08cad9c9ac
GERMAN IBARRA RODRIGUEZ <2024030429@upsin.edu.mx>
2026-07-08 23:03:43 -0700

    fix: install.sh fixed

--- install.sh
+++ install.sh
@@ -10,6 +10,9 @@ mkdir -p "$APP_DIR"
 curl -fsSL https://raw.githubusercontent.com/AlemanGerman/AutoQueuer/main/dist/manifest.json -o "$APP_DIR/manifest.json"
 curl -fsSL https://raw.githubusercontent.com/AlemanGerman/AutoQueuer/main/dist/icon.svg -o "$APP_DIR/icon.svg"
 curl -fsSL https://raw.githubusercontent.com/AlemanGerman/AutoQueuer/main/dist/index.js -o "$APP_DIR/index.js"
+curl -fsSL https://raw.githubusercontent.com/AlemanGerman/AutoQueuer/main/dist/extension.js -o "$APP_DIR/extension.js"
+curl -fsSL https://raw.githubusercontent.com/AlemanGerman/AutoQueuer/main/dist/style.css -o "$APP_DIR/style.css"
+
 
 spicetify config custom_apps AutoQueuer
 spicetify apply
